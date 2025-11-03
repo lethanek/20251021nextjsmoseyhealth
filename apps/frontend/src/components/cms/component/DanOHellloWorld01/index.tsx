9f93dde846a4fa7c975d90a8a943bdc0c75221f5
lethank <lethanek93@gmail.com>
2025-11-03 18:00:48 -0500

    cleaned up component

--- apps/frontend/src/components/cms/component/DanOHellloWorld01/index.tsx
+++ apps/frontend/src/components/cms/component/DanOHellloWorld01/index.tsx
@@ -9,9 +9,9 @@ export const DanOHellloWorld01Component : CmsComponent<DanOHellloWorld01DataFrag
     const componentName = 'Dan O Hello World 01'
     const componentInfo = ''
     return <CmsEditable className="w-full border-y border-y-solid border-y-slate-900 py-2 mb-4" {...editProps}>
-        <div className="font-bold italic">{ componentName }</div>
-        <div>{ componentInfo }</div>
-        { Object.getOwnPropertyNames(data).length > 0 && <pre className="w-full overflow-x-hidden font-mono text-sm bg-slate-200 p-2 rounded-sm border border-solid border-slate-900 text-slate-900">{ JSON.stringify(data, undefined, 4) }</pre> }
+        
+        <div>{ data?.Title }</div>
+
     </CmsEditable>
 }
 DanOHellloWorld01Component.displayName = "Dan O Hello World 01 (Component/DanOHellloWorld01)"
